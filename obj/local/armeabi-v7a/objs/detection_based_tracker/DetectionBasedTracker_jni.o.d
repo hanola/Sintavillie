bc04570d8811233f56e0764e1c8a1147e8fe82d2
Olav Brenna Hansen <post@vasetskiheiser.no>
2014-09-01 14:29:51 +0200

    First commit

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/Olav/NVPACK/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/Olav/NVPACK/android-ndk-r9d/platforms/android-8/arch-arm/usr/include/android/log.h:
